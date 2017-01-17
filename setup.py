f1950c998a17a969c137c109fdc32ab84eead1e7
alvations <alvations@gmail.com>
2017-01-17 13:24:44 +0800

    update tag version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+
 setup(
   name = 'charguana',
   packages = ['charguana'], 
-  version = '0.0.1a',
+  version = '0.1',
   description = 'A character vommitting library.',
   author = 'Liling Tan',
   author_email = 'alvations@gmail.com',
   url = 'https://github.com/alvations/charguana',
-  download_url = 'https://github.com/alvations/charguana/tarball/0.0.1a', 
+  download_url = 'https://github.com/alvations/charguana/tarball/0.1', 
   keywords = ['unicode', 'character', 'vommit', 'linguistics' ], 
   classifiers = [],
 )
