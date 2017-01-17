4cb3edd0dbc6bcd865612f0a9d4fc5036cfad243
alvations <alvations@gmail.com>
2017-01-17 12:51:15 +0800

    still trying to register

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'charguana',
   packages = ['charguana'], 
-  version = '0.0.1',
+  version = '0.0.1a',
   description = 'A character vommitting library.',
   author = 'Liling Tan',
-  author_email = 'abc@xyz.com',
+  author_email = 'alvations@gmail.com',
   url = 'https://github.com/alvations/charguana',
-  download_url = 'https://github.com/alvations/charguana/tarball/0.0.1', 
+  download_url = 'https://github.com/alvations/charguana/tarball/0.0.1a', 
   keywords = ['unicode', 'character', 'vommit', 'linguistics' ], 
   classifiers = [],
 )
