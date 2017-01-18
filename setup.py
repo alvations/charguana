6da6a0d769c78189477e42fdb06c335151350924
alvations <alvations@gmail.com>
2017-01-18 15:52:27 +0800

    upgrade to v0.1

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
   description = 'A character vommitting library.',
   author = 'Liling Tan',
   author_email = 'alvations@gmail.com',
+  license = 'MIT',
   url = 'https://github.com/alvations/charguana',
   download_url = 'https://github.com/alvations/charguana/tarball/0.1', 
   keywords = ['unicode', 'character', 'vommit', 'linguistics' ], 
