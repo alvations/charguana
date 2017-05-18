8441a5241319f6cc322f1b91eb13222120adf4ea
alvations <alvations@gmail.com>
2017-05-18 15:06:33 +0800

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'charguana',
   packages = ['charguana'],
-  version = '0.1.6',
+  version = '0.1.7',
   description = 'A character vomiting library.',
   author = 'Liling Tan',
   license = 'MIT',
