09d351cf50a3805dca1056bd121cc46794d480ba
alvations <alvations@gmail.com>
2017-05-18 14:35:48 +0800

    up version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
   name = 'charguana',
-  packages = ['charguana'], 
-  version = '0.1.4',
+  packages = ['charguana'],
+  version = '0.1.5',
   description = 'A character vomiting library.',
   author = 'Liling Tan',
   license = 'MIT',
   url = 'https://github.com/alvations/charguana',
-  download_url = 'https://github.com/alvations/charguana/tarball/0.1.3', 
-  keywords = ['unicode', 'character', 'vommit', 'linguistics' ], 
+  download_url = 'https://github.com/alvations/charguana/tarball/0.1.5',
+  keywords = ['unicode', 'character', 'vommit', 'linguistics' ],
   classifiers = [],
 )
